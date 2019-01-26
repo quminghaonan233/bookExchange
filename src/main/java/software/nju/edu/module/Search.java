a59b22cd2a26f6356e4a7b555dee7a9b252bdb90
huanghj <mf1832063@smail.nju.edu.cn>
2019-01-26 23:37:43 +0800

    testing for collaborators settings and creating the Search Module

---
+++ src/main/java/software/nju/edu/module/Search.java
@@ -0,0 +1,20 @@
+package software.nju.edu.module;
+
+public class Search {
+	
+	
+	// Lucene
+	String searchMethod;
+	String textField = "BOOK_TEXT";
+	String titleField = "BOOK_TITLE";
+	//Query query = null;
+	
+	int method = 0;
+	
+	
+	
+	
+	
+
+	
+}
