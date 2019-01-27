d249bf42a8e4df13081bd1c2c181d70d8babaf98
YellowBeans <singlexuan@vip.qq.com>
2019-01-27 23:54:46 +0800

    impl of UserRepository

--- src/main/java/software/nju/edu/domain/repository/UserRepositoryImpl.java
+++ src/main/java/software/nju/edu/domain/repository/UserRepositoryImpl.java
@@ -1,10 +1,45 @@
 package software.nju.edu.domain.repository;
 
+import java.util.List;
+
+import software.nju.edu.domain.entity.User;
+
 @Repository
 public class UserRepositoryImpl {
 	
+	private static String uuid = createUuid();
+	
 	private final ConcurrentMap<String, User> userConcurrentMap =
 			new ConcurrentMap<String, User>();
+	
+	
+	public User saveOrUpdateUser(User user) {
+		String uid = user.getUid();
+		if (uid == null) {
+			uid = uuid;
+		}
+		this.userConcurrentMap.put(uid, user);
+		return user;
+	}
+	
+	public void deleteUser(String uid) {
+		this.userConcurrentMap.remove(uid);
+		
+	}
+	
+	public User getUserById(String uid) {
+		return this.userConcurrentMap.get(uid);
+		
+	}
+	
+	public List<User> getUserList() {
+		return new ArrayList<User> (this.userConcurrentMap.values());
+	}
+	
+	public static String createUuid() {
+		// create unique uid
+		return "";
+	}
 	
 
 }
