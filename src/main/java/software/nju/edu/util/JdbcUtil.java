3e384af2b4020b23a08007fb5e5db35bef01ed73
YellowBeans <singlexuan@vip.qq.com>
2019-01-28 23:05:25 +0800

    add jdbc util

---
+++ src/main/java/software/nju/edu/util/JdbcUtil.java
@@ -0,0 +1,32 @@
+package software.nju.edu.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcUtil {
+	
+	private static Connection conn = null;
+	private static final String URL = "jdbc:mysql://127.0.0.1";
+	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String USERNAME = "test";
+	private static final String PASSWORD = "test";
+	public static Connection getConnection() {
+		try {
+			Class.forName(JDBC_DRIVER);// 找到 jdbc.Driver 驱动
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);// 加载驱动
+			
+		} catch (ClassNotFoundException e) { // 未找到驱动
+			e.printStackTrace();
+			
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public void closeAll() {
+		
+	}
+
+}
