e381cea86a55d92ca3b37b51ec50f1ac4ad6b645
huomozhihen <yq13yq13@sina.com>
2019-02-02 22:40:22 +0800

    message

--- src/main/java/software/nju/edu/controller/messageController.java
+++ src/main/java/software/nju/edu/controller/messageController.java
@@ -1,6 +1,5 @@
 package software.nju.edu.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,8 @@ import software.nju.edu.domain.entity.Trade;
 import software.nju.edu.domain.entity.TradeCancel;
 import software.nju.edu.mapper.TradeCancelMapper;
 import software.nju.edu.mapper.TradeMapper;
+import software.nju.edu.service.userService;
+import software.nju.edu.serviceimpl.userServiceImpl;
 
 @Controller
 public class messageController {
@@ -19,18 +20,36 @@ public class messageController {
 	private TradeMapper tradeMapper;
 	@Autowired
 	private TradeCancelMapper tradeCancelMapper;
-	
+	@Autowired
+	private userServiceImpl userService;
+
 	@RequestMapping("/myMessage")
-	public String myMessage(String uId, Model model){
+	public String myMessage(String uId, Model model) {
 		System.out.println(uId);
-		List<Trade> trade = tradeMapper.getAllTradesBySellerId(Integer.valueOf(uId));
-		System.out.println("trade Test=" + trade.get(0));
-		
+		List<Trade> sellList = tradeMapper.getAllTradesBySellerId(Integer.valueOf(uId));
+		System.out.println("sellList" + sellList.size());
+		List<Trade> buyList = tradeMapper.getAllTradesByBuyerId(Integer.valueOf(uId));
+		System.out.println("buyList" + buyList.size());
+
 		List<TradeCancel> tradeCancel = tradeCancelMapper.getAllTradeCancelBySourceId(Integer.valueOf(uId));
-		System.out.println("tradeCancel Test=" + tradeCancel.get(0));
+		System.out.println("tradeCancelSrc" + tradeCancel.size());
+		for(int i=0;i<tradeCancel.size();i++) {
+			Trade tradeTmp = tradeMapper.getTradeByTradeId(tradeCancel.get(i).gettId());
+			tradeCancel.get(i).setBookName(tradeTmp.getBookName());
+		}
 		
-		model.addAttribute(trade);
-		model.addAttribute(tradeCancel);
-	    return "message";
+		List<TradeCancel> tradeCancel2 = tradeCancelMapper.getAllTradeCancelByTargetId(Integer.valueOf(uId));
+		System.out.println("tradeCancelTar" + tradeCancel2.size());
+		for(int i=0;i<tradeCancel2.size();i++) {
+			Trade tradeTmp = tradeMapper.getTradeByTradeId(tradeCancel2.get(i).gettId());
+			tradeCancel2.get(i).setBookName(tradeTmp.getBookName());
+		}
+
+		model.addAttribute("sellList", sellList);
+		model.addAttribute("buyList", buyList);
+		model.addAttribute("tradeCancelTarList", tradeCancel2);
+		model.addAttribute("tradeCancelSrcList", tradeCancel);
+		model.addAttribute("user", userService.getUserById(Integer.parseInt(uId)));
+		return "message";
 	}
 }
