b35fba5988678e509701e5a6f19d9a7b12fe71ed
YellowBeans <singlexuan@vip.qq.com>
2019-02-03 04:30:57 +0800

    add myProfile.html, fix AddBookController and addBook.html, addBookResult.html, clean UserController.

--- src/main/java/software/nju/edu/controller/AddBookController.java
+++ src/main/java/software/nju/edu/controller/AddBookController.java
@@ -4,71 +4,103 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import software.nju.edu.domain.entity.Book;
 import software.nju.edu.serviceimpl.bookServiceImpl;
+import software.nju.edu.serviceimpl.userServiceImpl;
 import software.nju.edu.util.FileUtil;
 
 @Controller
 public class AddBookController {
-	
+
 	@Autowired
 	bookServiceImpl bookService;
-	
+
+	@Autowired
+	userServiceImpl userService;
+
 	@GetMapping("/addBook")
-	public String addBook() {
+	public String addBook(String uId, Model model) {
+		model.addAttribute("user", userService.getUserById(Integer.parseInt(uId)));
 		return "addBook";
 	}
-	
-	@PostMapping("/addBook")
-	public String upload(HttpServletRequest request) {
-		System.out.println("return...");
+
+	@PostMapping("/addBookResult")
+	public String upload(HttpServletRequest request, Model model) {
+
+		String addBookResultMessage = "add New Book Successfully";
+
 		MultipartHttpServletRequest params = ((MultipartHttpServletRequest) request);
-		MultipartFile file = ((MultipartHttpServletRequest) request).getFile("file");
+		MultipartFile file = params.getFile("file");
+		String uId = params.getParameter("uId");
+
 		String fileName = file.getOriginalFilename();
 		String filePath = "/Users/huanghj/img/";
-		
-		int bId = Integer.valueOf(params.getParameter("bId"));
-		String bookName = params.getParameter("bookName");
-		String book_owner = params.getParameter("book_owner");
-		String publisher = params.getParameter("publisher");
-		String author = params.getParameter("author");
-		String newDegree = params.getParameter("author");
-		String address = params.getParameter("author");
-		int onsale = Integer.valueOf(params.getParameter("onsale"));
-		int isDel = Integer.valueOf(params.getParameter("isDel"));
-		int price = Integer.valueOf(params.getParameter("price"));
-		String img = filePath + fileName;
-		
-		Book book = new Book();
-		book.setbId(bId);
-		book.setBookName(bookName);
-		book.setBook_owner(book_owner);
-		book.setPublisher(publisher);
-		book.setAuthor(author);
-		book.setNewDegree(newDegree);
-		book.setAddress(address);
-		book.setOnsale(onsale);
-		book.setIsDel(isDel);
-		book.setPrice(price);
-		book.setImg(img);
-		
-		bookService.addNewBook(book);
-		
+
+		int onsale = 0;
+		int isDel = 0;
+		int price = 0;
+		String bookName = null;
+		String book_owner = null;
+		String publisher = null;
+		String author = null;
+		String newDegree = null;
+		String address = null;
+		String img = null;
+
 		try {
-			FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+			bookName = params.getParameter("bookName");
+			book_owner = params.getParameter("book_owner");
+			publisher = params.getParameter("publisher");
+			author = params.getParameter("author");
+			newDegree = params.getParameter("author");
+			address = params.getParameter("author");
+			onsale = Integer.valueOf(params.getParameter("onsale"));
+			isDel = Integer.valueOf(params.getParameter("isDel"));
+			price = Integer.valueOf(params.getParameter("price"));
+			img = filePath + fileName;
+
+			try {
+				Book book = new Book();
+				book.setBookName(bookName);
+				book.setBook_owner(book_owner);
+				book.setPublisher(publisher);
+				book.setAuthor(author);
+				book.setNewDegree(newDegree);
+				book.setAddress(address);
+				book.setOnsale(onsale);
+				book.setIsDel(isDel);
+				book.setPrice(price);
+				book.setImg(img);
+
+				try {
+					// upload file: book image.
+					FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+					// only then did bookService add New Book.
+					bookService.addNewBook(book);
+
+				} catch (Exception e) {
+					addBookResultMessage = "add New Book Unsuccessfully : When upload image.";
+
+				}
+
+			} catch (Exception e) {
+				addBookResultMessage = "add New Book Unsuccessfully : When write to database.";
+			}
+
 		} catch (Exception e) {
-			
+			addBookResultMessage = "add New Book Unsuccessfully : When get params.";
+
 		}
-		
-		return "myBook";
+
+		model.addAttribute("user", userService.getUserById(Integer.parseInt(uId)));
+		model.addAttribute("addBookResultMessage", addBookResultMessage);
+		return "addBookResult";
 	}
 
 }
--- src/main/java/software/nju/edu/controller/UserController.java
+++ src/main/java/software/nju/edu/controller/UserController.java
@@ -1,21 +1,5 @@
 package software.nju.edu.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class UserController {
-	
-	@RequestMapping("/user")
-	public String getUser() {
-		return "User managed center";
-	}
-	
-	@RequestMapping("/user/{uId}")
-	public String getUser(@PathVariable int uId) {
-		return "uId = " + uId;
-	}
-	
 
 }
