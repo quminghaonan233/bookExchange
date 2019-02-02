2fd5d21f796db3a6eeac1dc4bc3131dfaa20475c
huomozhihen <yq13yq13@sina.com>
2019-02-02 15:23:15 +0800

    test

--- src/main/java/software/nju/edu/controller/messageController.java
+++ src/main/java/software/nju/edu/controller/messageController.java
@@ -1,16 +1,32 @@
 package software.nju.edu.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import software.nju.edu.domain.association.TradeCancel;
+import software.nju.edu.domain.entity.Trade;
+import software.nju.edu.mapper.TradeCancelMapper;
+import software.nju.edu.mapper.TradeMapper;
 
 
 
 @Controller
 public class messageController {
-	
+	@Autowired
+	private TradeMapper tradeMapper;
+	private TradeCancelMapper tradeCancelMapper;
 	
 	@RequestMapping("/myMessage")
-	public String hello(String uId){
+	public String myMessage(String uId, Model model){
+		System.out.println(uId);
+//		List<Trade> trade = tradeMapper.getAllTradesByUserId(Integer.valueOf(uId));
+//		List<TradeCancel> tradeCancel = tradeCancelMapper.getAllTradeCancelByUserId(Integer.valueOf(uId));
+//		model.addAttribute(trade);
+//		model.addAttribute(tradeCancel);
 	    return "message";
 	}
 }
