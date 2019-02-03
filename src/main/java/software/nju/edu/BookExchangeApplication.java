9f3990d7bf24abd2e04fa7fb4d90da93d051fa5c
quminghaonan233 <131250065@smail.nju.edu.cn>
2019-02-03 18:42:07 +0800

    打包

--- src/main/java/software/nju/edu/BookExchangeApplication.java
+++ src/main/java/software/nju/edu/BookExchangeApplication.java
@@ -3,15 +3,24 @@ package software.nju.edu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 @MapperScan("software.nju.edu.mapper")
-public class BookExchangeApplication {
+public class BookExchangeApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookExchangeApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(BookExchangeApplication.class);
+    }
+
 
 }
 
