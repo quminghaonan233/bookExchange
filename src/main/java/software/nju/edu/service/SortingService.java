3b7f44d255824bc1c5c1027e3d771661ca231f6a
YellowBeans <singlexuan@vip.qq.com>
2019-02-17 23:21:56 +0800

    add more interfaces to sortingService.

--- src/main/java/software/nju/edu/service/SortingService.java
+++ src/main/java/software/nju/edu/service/SortingService.java
@@ -6,10 +6,32 @@ import software.nju.edu.domain.entity.Book;
 
 public interface SortingService {
 	
-	List<Book> sortedByPrice(List<Book> bookList);
+	List<Book> sortedByHotIndexASC(List<Book> bookList);
 	
-	List<Book> sortedByCredit(List<Book> bookList);
+	List<Book> sortedByHotIndexDESC(List<Book> bookList);
 	
-	List<Book> sortedByTimestamp(List<Book> bookList);
+	List<Book> sortedByClicksASC(List<Book> bookList);
+	
+	List<Book> sortedByClicksDESC(List<Book> bookList);
+	
+	List<Book> sortedByViewsASC(List<Book> bookList);
+	
+	List<Book> sortedByViewsDESC(List<Book> bookList);
+	
+	List<Book> sortedByClickThroughRateASC(List<Book> bookList);
+	
+	List<Book> sortedByClickThroughRateDESC(List<Book> bookList);
+	
+	List<Book> sortedByPriceASC(List<Book> bookList);
+	
+	List<Book> sortedByPriceDESC(List<Book> bookList);
+	
+	List<Book> sortedByCreditASC(List<Book> bookList);
+	
+	List<Book> sortedByCreditDESC(List<Book> bookList);
+	
+	List<Book> sortedByTimeASC(List<Book> bookList);
+	
+	List<Book> sortedByTimeDESC(List<Book> bookList);
 
 }
--- src/main/java/software/nju/edu/service/impl/SortingServiceImpl.java
+++ src/main/java/software/nju/edu/service/impl/SortingServiceImpl.java
@@ -17,8 +17,14 @@ public class SortingServiceImpl implements SortingService {
 	@Autowired
 	BookServiceImpl bookService;
 	
+	
+	/**
+	 * 按价格升序
+	 * @param bookList
+	 * @return
+	 */
 	@Override
-	public List<Book> sortedByPrice(List<Book> bookList) {
+	public List<Book> sortedByPriceASC(List<Book> bookList) {
 		// TODO Auto-generated method stub
 		Collections.sort(bookList, new Comparator<Book>() {
 			public int compare(Book b1, Book b2) {
@@ -33,7 +39,12 @@ public class SortingServiceImpl implements SortingService {
 		return bookList;
 	}
 	
-	public List<Book> sortedByPriceReverse(List<Book> bookList) {
+	/**
+	 * 按价格降序
+	 * @param bookList
+	 * @return
+	 */
+	public List<Book> sortedByPriceDESC(List<Book> bookList) {
 		// TODO Auto-generated method stub
 		Collections.sort(bookList, new Comparator<Book>() {
 			public int compare(Book b1, Book b2) {
@@ -48,9 +59,9 @@ public class SortingServiceImpl implements SortingService {
 		return bookList;
 	}
 	
-
+	
 	@Override
-	public List<Book> sortedByCredit(List<Book> bookList) {
+	public List<Book> sortedByCreditDESC(List<Book> bookList) {
 		// TODO Auto-generated method stub
 		Collections.sort(bookList, new Comparator<Book>() {
 			public int compare(Book b1, Book b2) {
@@ -66,7 +77,7 @@ public class SortingServiceImpl implements SortingService {
 	}
 
 	@Override
-	public List<Book> sortedByTimestamp(List<Book> bookList) {
+	public List<Book> sortedByTimeASC(List<Book> bookList) {
 		// TODO Auto-generated method stub
 		Collections.sort(bookList, new Comparator<Book>() {
 			public int compare(Book b1, Book b2) {
@@ -79,4 +90,17 @@ public class SortingServiceImpl implements SortingService {
 		
 	}
 	
+	@Override
+	public List<Book> sortedByTimeDESC(List<Book> bookList) {
+		// TODO Auto-generated method stub
+		Collections.sort(bookList, new Comparator<Book>() {
+			public int compare(Book b1, Book b2) {
+				Date d1 = b1.getFinalUpdateTime();
+				Date d2 = b2.getFinalUpdateTime();
+				return d1.compareTo(d2);
+			}
+		});
+		return bookList;
+		
+	}
 }
