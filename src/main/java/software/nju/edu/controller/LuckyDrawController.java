5491639ade8bb918a7312a7cebb483c4417a43c7
YellowBeans <singlexuan@vip.qq.com>
2019-03-20 11:20:46 +0800

    完善幸运抽奖模式

--- src/main/java/software/nju/edu/controller/LuckyDrawController.java
+++ src/main/java/software/nju/edu/controller/LuckyDrawController.java
@@ -1,5 +1,7 @@
 package software.nju.edu.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,13 +20,60 @@ public class LuckyDrawController {
 		// 100 score once.
 		// 20 * 0.1 + 50 * 0.3 + 100 * 0.5 + 200 * 0.09 + 1000 * 0.01 = 
 		// 2 + 15 + 50 + 18 + 10 = 97 
-		double[] probability = {0.2, 0.3, 0.4, 0.09, 0.01};
-		int[] score = {20, 50, 100, 200, 1000};
+		double[] probArray = {0.2, 0.3, 0.4, 0.09, 0.01};
+		int[] prizeArray = {20, 50, 100, 200, 1000};
 		
-		model.addAttribute("probability", probability);
-		model.addAttribute("score", score);
+		model.addAttribute("probArray", probArray);
+		model.addAttribute("prizeArray", prizeArray);
 		model.addAttribute("user", userService.getUserById(Integer.parseInt(uId)));
 		return "/luckyDraw";
+	}
+	
+	@GetMapping("/luckyDrawResult")
+	public String luckyDrawResult(String uId, Model model) {
+		// 积分扣除
+		
+		
+		double[] prob = {0.2, 0.3, 0.4, 0.09, 0.01};
+		int[] prize = {20, 50, 100, 200, 1000};
+		int prizeResult = randomDraw(prob, prize);
+		
+		// 积分增加
+		model.addAttribute("prizeResult", prizeResult);
+		model.addAttribute("user", userService.getUserById(Integer.parseInt(uId)));
+		return "/luckyDrawResult";
+		
+	}
+	
+	public int randomDraw(double[] prob, int[] prize) {
+		Random rand = new Random();
+		int result = 0;
+		int a = rand.nextInt(100);
+		int b = ((a<20)==true?1:0) 
+				+ ((a<50)==true?1:0)
+				+((a<90)==true?1:0)
+				+((a<99)==true?1:0)
+				+((a<100)==true?1:0);
+		System.out.println(a);
+		switch(b) {
+		case 5:
+			result = prize[0];
+			break;
+		case 4:
+			result = prize[1];
+			break;
+		case 3:
+			result = prize[2];
+			break;
+		case 2:
+			result = prize[3];
+			break;
+		case 1:
+			result = prize[4];
+			break;
+		
+		}
+		return result;
 	}
 
 }
