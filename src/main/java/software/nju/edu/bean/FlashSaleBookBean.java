e8eabe576eff95ca8be82ab8d1f0a5b62577ca1e
huomozhihen <yq13yq13@sina.com>
2019-03-27 10:34:29 +0800

    purchaseAndSaleChange

--- src/main/java/software/nju/edu/bean/FlashSaleBookBean.java
+++ src/main/java/software/nju/edu/bean/FlashSaleBookBean.java
@@ -13,6 +13,102 @@ public class FlashSaleBookBean {
 	private String author;
 	private String newDegree;
 	private String address;
+	public int getbId() {
+		return bId;
+	}
+	public void setbId(int bId) {
+		this.bId = bId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public int getBook_owner() {
+		return book_owner;
+	}
+	public void setBook_owner(int book_owner) {
+		this.book_owner = book_owner;
+	}
+	public String getBookType() {
+		return bookType;
+	}
+	public void setBookType(String bookType) {
+		this.bookType = bookType;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getNewDegree() {
+		return newDegree;
+	}
+	public void setNewDegree(String newDegree) {
+		this.newDegree = newDegree;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getOnsale() {
+		return onsale;
+	}
+	public void setOnsale(int onsale) {
+		this.onsale = onsale;
+	}
+	public int getIsDel() {
+		return isDel;
+	}
+	public void setIsDel(int isDel) {
+		this.isDel = isDel;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public Timestamp getFinalUpdateTime() {
+		return finalUpdateTime;
+	}
+	public void setFinalUpdateTime(Timestamp finalUpdateTime) {
+		this.finalUpdateTime = finalUpdateTime;
+	}
+	public int getFlashSalePrice() {
+		return flashSalePrice;
+	}
+	public void setFlashSalePrice(int flashSalePrice) {
+		this.flashSalePrice = flashSalePrice;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
 	private int onsale;
 	private int isDel;
 	private int price;
--- src/main/java/software/nju/edu/domain/entity/purchaseInfo.java
+++ src/main/java/software/nju/edu/domain/entity/purchaseInfo.java
@@ -3,7 +3,7 @@ package software.nju.edu.domain.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Trade implements Serializable {
+public class purchaseInfo implements Serializable{
 	/**
 	 * version
 	 */
@@ -13,6 +13,81 @@ public class Trade implements Serializable {
 	 * Attributes
 	 */
 	private int tId;           // Primary key
+	public int gettId() {
+		return tId;
+	}
+	public void settId(int tId) {
+		this.tId = tId;
+	}
+	public int getBuyerId() {
+		return buyerId;
+	}
+	public void setBuyerId(int buyerId) {
+		this.buyerId = buyerId;
+	}
+	public String getBuyerName() {
+		return buyerName;
+	}
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+	public int getSellerId() {
+		return sellerId;
+	}
+	public void setSellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+	public String getSellerName() {
+		return sellerName;
+	}
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+	public int getbId() {
+		return bId;
+	}
+	public void setbId(int bId) {
+		this.bId = bId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public Date getStart() {
+		return start;
+	}
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	public String getSendTo() {
+		return sendTo;
+	}
+	public void setSendTo(String sendTo) {
+		this.sendTo = sendTo;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	private int buyerId;       // Foreign key: uId
 	private String buyerName; 
 	private int sellerId;      // Foreign key: uId
@@ -24,131 +99,4 @@ public class Trade implements Serializable {
 	private String sendTo;
 	private String description;
 	private int status;
-
-
-	// constructor without argument.
-	public Trade() {
-
-	}
-
-	public Trade(int buyerId, String buyerName, int sellerId, String sellerName, int bId, String bookName, Date start,
-			String sendTo, String description, int status) {
-		super();
-		this.buyerId = buyerId;
-		this.buyerName = buyerName;
-		this.sellerId = sellerId;
-		this.sellerName = sellerName;
-		this.bId = bId;
-		this.bookName = bookName;
-		this.start = start;
-		this.sendTo = sendTo;
-		this.description = description;
-		this.status = status;
-	}
-
-	// constructor with several arguments.
-	public Trade(int tId, int buyerId, int sellerId, int bId) {
-		this.tId = tId;
-		this.buyerId = buyerId;
-		this.sellerId = sellerId;
-		this.bId = bId;
-	}
-
-	// getters and setters
-	public int gettId() {
-		return tId;
-	}
-
-	public void settId(int tId) {
-		this.tId = tId;
-	}
-
-	public int getBuyerId() {
-		return buyerId;
-	}
-
-	public void setBuyerId(int buyerId) {
-		this.buyerId = buyerId;
-	}
-
-	public String getBuyerName() {
-		return buyerName;
-	}
-
-	public void setBuyerName(String buyerName) {
-		this.buyerName = buyerName;
-	}
-
-	public int getSellerId() {
-		return sellerId;
-	}
-
-	public void setSellerId(int sellerId) {
-		this.sellerId = sellerId;
-	}
-
-	public String getSellerName() {
-		return sellerName;
-	}
-
-	public void setSellerName(String sellerName) {
-		this.sellerName = sellerName;
-	}
-
-	public int getbId() {
-		return bId;
-	}
-
-	public void setbId(int bId) {
-		this.bId = bId;
-	}
-
-	public String getBookName() {
-		return bookName;
-	}
-
-	public void setBookName(String bookName) {
-		this.bookName = bookName;
-	}
-
-	public Date getStart() {
-		return start;
-	}
-
-	public void setStart(Date start) {
-		this.start = start;
-	}
-
-	public Date getEnd() {
-		return end;
-	}
-
-	public void setEnd(Date end) {
-		this.end = end;
-	}
-
-	public String getSendTo() {
-		return sendTo;
-	}
-
-	public void setSendTo(String sendTo) {
-		this.sendTo = sendTo;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	
 }
