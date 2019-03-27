7fe496a22630f713b28f526765c0ce0954f0a1ab
quminghaonan233 <131250065@smail.nju.edu.cn>
2019-03-27 21:47:52 +0800

    lei

---
+++ src/main/java/software/nju/edu/bean/ModifyPassword.java
@@ -0,0 +1,26 @@
+package software.nju.edu.bean;
+
+public class ModifyPassword {
+	private String userId;
+	private String password;
+	private String newPassword;
+	
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
